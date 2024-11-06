198a0763102046d1f18bf70364f29c01278ee7ac
JavFuertes <j.fuertesguadarrama@student.tudelft.nl>
2024-11-06 11:16:10 +0100

    __init__ in analytical change

--- bricks/analytical/__init__.py
+++ bricks/analytical/__init__.py
@@ -1,4 +1,2 @@
 from .house import house
-from .assessment.ltsm import LTSM
-from .assessment.emethods import EM
-from .assessment.damage_parameter import compute_damage_parameter
+from .assessment import *
